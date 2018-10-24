c2e4f6b309a5c81eda942b6ac68857c3ebc580ac
emmeiwhite <emmeiwhite@gmail.com>
2018-10-24 19:38:29 +0530

    Routing in React

---
+++ src/components/child.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Child extends Component{
+    render(){
+        return(
+            <h1>THE CHILD COMPONENT</h1>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Router,Route,browserHistory,Link,IndexRoute} from 'react-router';
+
 import App from './App';
+import Child from './components/child';
+
+import './index.css';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// Let's create some Components- Functional Components and Learn Nested Routes
+ReactDOM.render(
+    <Router history={browserHistory}>
+        <Route path="/" component={App} />
+        <Route path="/child" component={Child} />
+        {/* Nested Route*/}
+        <Route>
+
+        </Route>
+    </Router>
+   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
