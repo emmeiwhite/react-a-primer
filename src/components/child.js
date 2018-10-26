87ff37681111ad31181613e272d58dc439977b4a
emmeiwhite <emmeiwhite@gmail.com>
2018-10-26 19:35:44 +0530

    Modal Added within Child Component

--- src/components/child.js
+++ src/components/child.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import ModalPopUp from './modal-example';
 
 export default class Child extends Component{
     render(){
         return(
-            <h1>THE CHILD COMPONENT</h1>
+            <div>
+                 <h1>THE CHILD COMPONENT</h1>
+                 {/* 26-10-2018 */}
+                 <ModalPopUp/>
+            </div>
+           
         )
     }
 }
--- src/components/modal-example.js
+++ src/components/modal-example.js
@@ -24,8 +24,12 @@ class ModalPopUp extends Component{
         return(
             <div>
                     <button onClick={this.modalClickHandler}>Show Modal</button>
-                    <Modal isOpen={this.state.isActive}>
-                        Hello This is World's Awesonme Awesonme Awesome Modal !!!
+                    <Modal isOpen={this.state.isActive} onRequestClose={this.modalClickHandler}>
+                        <h1>Hello This is World's Awesonme Awesonme Awesome Modal !!!</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident maiores quos, ad quasi impedit cupiditate incidunt minus, vel vero ea saepe sit dolor mollitia officiis consequatur qui nam doloremque alias tempore veniam ut voluptatum ex quis repudiandae! Officia, ullam optio recusandae ducimus praesentium accusantium tempora. Obcaecati voluptate nemo iusto eius laborum vitae cupiditate voluptatibus distinctio architecto, dignissimos tenetur labore aut! Assumenda accusantium praesentium reprehenderit rem repudiandae, aperiam et ad. Officia, ut nesciunt. Voluptatem fugit beatae nihil ratione doloremque est tempora possimus dolores
+                         libero in repellendus tenetur eos, placeat ducimus voluptates repudiandae corporis. Fugiat praesentium tempora, corrupti ut optio rem rerum.</p>
+
+                        <button onClick={this.modalClickHandler}>Hide Modal</button>
                     </Modal>
             </div>
           
