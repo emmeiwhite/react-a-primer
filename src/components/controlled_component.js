051f5cc6d25110abd42772b6391ed0fb7c8436c4
emmeiwhite <emmeiwhite@gmail.com>
2018-10-29 20:48:26 +0530

    button desabled based on state

--- src/components/controlled_component.js
+++ src/components/controlled_component.js
@@ -5,21 +5,34 @@ export default class ControlledForm extends Component{
         super(props);
         
         this.state = {
-            goodPeople : []
+            name : ''
         }
     }
 
     handleSubmit = ()=>{
-        console.dir(this._name)
+        console.dir(this.state.name)
     }
+
+    nameChangehandler = (event)=>{
+        this.setState({
+            name:event.target.value
+        })
+
+        console.log(this.state.name)
+    }
+
     render(){
         return(
             <div>
                 <form onSubmit={event=>event.preventDefault()}>
                     <input 
                       type="text" 
-                      ref={(a)=>this._name = a}  /> {/* this._name is an object of <input> available throughout this Component now!*/}
-                    <button type="submit" onClick={this.handleSubmit}>Submit Form</button>
+                      value={this.state.name} 
+                      onChange = {this.nameChangehandler}/> {/* ref={(a)=>this._name = a}  this._name is an object of <input> available throughout this Component now!*/}
+                    <button 
+                        type="submit" 
+                        disabled={this.state.name.length ? false : true}
+                        onClick={this.handleSubmit}>Submit Form</button>
                 </form>
             </div>
         )
